8b509fb416ff0463302a08fad1bba6b47af3a620
ZITOUNI Nidhal <nidhalzitouni111@gmail.com>
2026-03-12 23:58:30 +0100

    refactor: remove docstring from __init__.py in evaluation module

--- src/xai_clinical/evaluation/__init__.py
+++ src/xai_clinical/evaluation/__init__.py
@@ -1,15 +1,3 @@
-"""
-Evaluation module.
-
-Provides tools for building and training classifiers.
-
-Classes:
-    ClassifierFactory: Factory for creating classifier instances.
-    ModelTrainer: Handles model training workflows.
-
-Usage:
-    from evaluation import ClassifierFactory, ModelTrainer
-"""
 
 from .classifiers import ClassifierFactory
 from .trainer import ModelTrainer
